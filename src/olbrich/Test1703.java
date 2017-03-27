187862469f275ad25bfd362f3153a6fba197cff7
molbrich <molbrich@student.tgm.ac.at>
2017-03-27 10:46:54 +0200

    commit

--- src/olbrich/Test1703.java
+++ src/olbrich/Test1703.java
@@ -5,6 +5,8 @@ public class Test1703 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hallo Miguel");
+		System.out.println("Hallo Miguel");
+		System.out.println("Hallo Miguel");
 		
 	}
 
